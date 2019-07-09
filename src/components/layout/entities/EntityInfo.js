8da4832957c51304068e40afcb88f5714de239d0
Davide Pirola <ziocleto@gmail.com>
2019-07-09 17:26:39 +0100

    Portal layout for more complex entity ready

---
+++ src/components/layout/entities/EntityInfo.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+const moment = require("moment");
+
+const EntityInfo = ({ currentEntity }) => {
+  const creationDate =
+    currentEntity && currentEntity.entity.metadata.creationDate
+      ? moment(currentEntity.entity.metadata.creationDate).fromNow()
+      : "1st Jan 1970";
+  const updateDate =
+    currentEntity &&
+    currentEntity.entity.metadata.lastUpdatedDate &&
+    currentEntity.entity.metadata.lastUpdatedDate !==
+      currentEntity.entity.metadata.creationDate
+      ? moment(currentEntity.entity.metadata.lastUpdatedDate).fromNow()
+      : "Never";
+
+  return (
+    <div className="entity-info-a">
+      <p>
+        <i className="fas fa-info-circle"> </i> Info
+      </p>
+      <div className="entity-info-c">
+        <span
+          className="metaInfiTitle small text-pale"
+          style={{ minWidth: "300px" }}
+        >
+          <i className="fas fa-user" />
+          &nbsp;owner&nbsp;
+        </span>
+        <span className="metaInfoValue normal text-secondary">
+          {currentEntity.entity.metadata.creator
+            ? currentEntity.entity.metadata.creator.name
+            : "Unknown"}
+        </span>
+        <br />
+        <span
+          className="metaInfiTitle small text-pale"
+          style={{ minWidth: "300px" }}
+        >
+          <i className="fas fa-clock" />
+          &nbsp;created&nbsp;
+        </span>
+        <span className="metaInfoValue normal text-secondary">
+          {creationDate}
+        </span>
+        <br />
+        <span className="metaInfiTitle small text-pale">
+          <i className="fas fa-calendar" />
+          &nbsp;updated&nbsp;
+        </span>
+        <span className="metaInfoValue normal text-secondary">
+          {updateDate}
+        </span>
+        <br />
+        <span className="metaInfiTitle small text-pale">
+          <i className="fas fa-code" />
+          &nbsp;hash
+        </span>
+        <span className="metaInfoValue normal text-secondary">
+          {currentEntity.entity.metadata.hash}
+        </span>
+        <br />
+      </div>
+    </div>
+  );
+};
+
+EntityInfo.propTypes = {
+  currentEntity: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+  currentEntity: state.entities.currentEntity
+});
+
+export default connect(mapStateToProps)(EntityInfo);
--- src/components/layout/entities/EntriesEditor.js
+++ src/components/layout/entities/EntriesEditor.js
@@ -6,27 +6,13 @@ import { showConfirmAlert } from "../../../actions/confirmalert";
 import ConfirmAlert from "../ConfirmAlert";
 import EntityUpdateContent from "./EntityUpdateContent";
 import EntityTags from "./EntityTags";
-import EntityCreator from "./EntityCreator";
-
-const moment = require("moment");
+import EntityInfo from "./EntityInfo";
 
 const EntriesEditor = ({ currentEntity, loading, showConfirmAlert }) => {
   const onDeleteEntity = e => {
     // e.preventDefault();
     showConfirmAlert("Confirm deletion of ", "danger");
   };
-
-  const creationDate =
-    currentEntity && currentEntity.entity.metadata.creationDate
-      ? moment(currentEntity.entity.metadata.creationDate).fromNow()
-      : "1st Jan 1970";
-  const updateDate =
-    currentEntity &&
-    currentEntity.entity.metadata.lastUpdatedDate &&
-    currentEntity.entity.metadata.lastUpdatedDate !==
-      currentEntity.entity.metadata.creationDate
-      ? moment(currentEntity.entity.metadata.lastUpdatedDate).fromNow()
-      : "Never";
 
   const entityRender =
     currentEntity === null ? (
@@ -38,31 +24,12 @@ const EntriesEditor = ({ currentEntity, loading, showConfirmAlert }) => {
         </div>
         <EntityUpdateContent />
         <EntityTags />
-        <EntityCreator />
-        <div className="entity-dates-a">
-          <p>
-            <i className="fas fa-calendar"> </i> Dates
-          </p>
-          <p className="react-tags">
-            <span className="small text-pale" style={{ minWidth: "300px" }}>
-              created:&nbsp;
-            </span>
-            <span className="text-secondary">{creationDate}</span>
-            <br />
-            <span className="small text-pale">updated:&nbsp;</span>
-            <span className="text-secondary">{updateDate}</span>
-          </p>
-        </div>
-        <div />
-        <div className="metaName-a normal">Name:</div>
-        <div className="metaValue-a medium text-primary">
+        <EntityInfo />
+        <div className="nameValue-a medium text-primary">
           {currentEntity.entity.metadata.name}
         </div>
         <ConfirmAlert />
-        <div className="metaHash-a normal">Hash:</div>
-        <div className="metaValueHash-a normal text-pale">
-          {currentEntity.entity.metadata.hash}
-        </div>
+        <div />
         <div className="deleteentity-a">
           <input
             type="button"
