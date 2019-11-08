29c643e2a3ed1e6f80f1ae0f5021bd9c6a50749a
Davide Pirola <ziocleto@gmail.com>
2019-11-08 16:08:18 +0000

    config.yml now deploys EH portal with wasm included.

--- public/editor.js
+++ public/editor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bf1f96a1d97835aafd6000789d701c9a4aacac5d3646439e1841fcdaea611d3
+oid sha256:d5053a57033cf0b96f87ba010d8730e1166e9b6254da2c4a375f861550c60a49
 size 776672
