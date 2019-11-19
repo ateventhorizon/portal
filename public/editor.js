4993953657e0c0dea8300dc251156d0c5d5b5a8a
Davide Pirola <ziocleto@gmail.com>
2019-11-19 18:37:54 +0000

    New restful-api infrastructure, with docker-compose, improved http server and websocket agnosticity

--- public/editor.js
+++ public/editor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1720e7059dd0cfcdad7f52360db70f88a4997dd0d0efc6fa43c0ffc6a9719f58
+oid sha256:428544f2a93b80d190775071869645e79b4728bb67313f23a5376ef9439a7f8a
 size 775909
