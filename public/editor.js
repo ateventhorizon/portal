78b2333b9407b3e243790433e397c5f462302cba
Davide Pirola <ziocleto@gmail.com>
2020-01-10 03:09:24 +0000

    Some fixes for emscripten version, bring it on!

--- public/editor.js
+++ public/editor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:665829cb88b99222899b31fb38e509ac2c57780746e1a5a17871c31681e90552
-size 775909
+oid sha256:3ef9350b3a7f5cb4dae5ce1b2e2b03b3c527d3d481ef6f9f2d27f065c04dafcd
+size 775987
