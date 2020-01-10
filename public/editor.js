69e89b0a105d24d92f16a1293e1f4501f99ded2a
Davide Pirola <ziocleto@gmail.com>
2020-01-10 20:36:15 +0000

    more visual fixes including shadowmaps and bad N in fragment shaders

--- public/editor.js
+++ public/editor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ef9350b3a7f5cb4dae5ce1b2e2b03b3c527d3d481ef6f9f2d27f065c04dafcd
+oid sha256:e15e23db9b15eac730ce7fcb52782ca1710c88559262aea428a6c39ced217e68
 size 775987
