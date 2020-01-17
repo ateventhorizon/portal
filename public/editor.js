edadd6f267e246b8fc0513db0df495c6d463e98d
Davide Pirola <ziocleto@gmail.com>
2020-01-17 02:22:49 +0000

    Fixed per-material messaging series of bugs, including few imageUtils rescale bugs

--- public/editor.js
+++ public/editor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce84aaf1eb10e9b4b16a762a0a1d6218c89f5ff8f03b397f5b471c37662f6bed
+oid sha256:dce9097741ca3412e958a2945fbf13588b35e490487b851678138cac5f837c5b
 size 775929
