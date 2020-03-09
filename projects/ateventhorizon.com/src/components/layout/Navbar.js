589fc57586b57eb77a90f9d4eb674bbd330b5cab
DadoLinux <ziocleto@gmail.com>
2020-03-09 18:09:06 +0000

    optimising navbar component, wow

--- projects/ateventhorizon.com/src/components/layout/Navbar.js
+++ projects/ateventhorizon.com/src/components/layout/Navbar.js
@@ -1,34 +1,30 @@
 import React, {Fragment} from "react";
 import {ProgressBar} from "../../futuremodules/progressbar/ProgressBar";
-import {UserNameText} from "./Navbar.styled";
+import {Navbareh, NavbarGrid, NavbarLogo, NavbarTitle, UserNameText} from "./Navbar.styled";
 import {getProject, getUserName, logoffFromProject, useGetAuth} from "../../futuremodules/auth/authAccessors";
 
 const Navbar = () => {
 
   const auth = useGetAuth();
   const userName = getUserName(auth);
-  const title = getProject(auth);
 
   return (
     <Fragment>
       <ProgressBar/>
-      <div className="navbarGrid">
-        <div className="navbarlogo-a">
+      <NavbarGrid>
+        <NavbarLogo>
           <img src="/ehlogo.svg" alt=""/>
-        </div>
-        <div className="navbareh-a navdiv-titletext">
-          {" "}
+        </NavbarLogo>
+        <Navbareh>
           <span className="colorLogo1">E</span>
           <span>vent</span> <span className="colorLogo2">H</span>
           <span>orizon</span>
-        </div>
-        <div className="navbartitle-a">{title}</div>
-        <UserNameText onClick={ () =>
-          logoffFromProject(auth)
-        }>
+        </Navbareh>
+        <NavbarTitle>{getProject(auth)}</NavbarTitle>
+        <UserNameText onClick={() => logoffFromProject(auth) }>
           {userName && <span><i className="fas fa-user"/>{" "}{userName}</span>}
         </UserNameText>
-      </div>
+      </NavbarGrid>
     </Fragment>
   );
 };
