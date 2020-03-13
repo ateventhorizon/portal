e0e8befc8775b3f17fcf92d59b16012251ce0017
DadoLinux <ziocleto@gmail.com>
2020-03-13 13:24:34 +0000

    removing futuremodules it will temporarily fail the build :/

--- projects/ateventhorizon.com/src/futuremodules/config/HostEnv.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const initHostEnv = () => {
-  axios.defaults.withCredentials = true;
-  axios.defaults.validateStatus = (status) => {
-    return status < 500; // Reject only if the status code is greater than or equal to 500
-  }
-};
